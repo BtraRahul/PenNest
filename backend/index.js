fb0aad8f5e1b9ae1aac60c6a3074e01246498e6b
Rahul Batra <97456632+BtraRahul@users.noreply.github.com>
2024-06-29 03:15:24 +0530

    Cors fixed

--- backend/index.js
+++ backend/index.js
@@ -17,19 +17,19 @@ const CLIENT_URL = process.env.CLIENT_URL;
 const app = express();
 
 //option 1: allow all origins with default of cors(*)
-// app.use(cors());
-app.use(
-  cors({
-    credentials: true,
-    origin: ["*",
-      "http://localhost:5173",
-      "http://localhost:5555",
-      API_BASE_URL,
-      CLIENT_URL,
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
+app.use(cors());
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: ["*",
+//       "http://localhost:5173",
+//       "http://localhost:5555",
+//       API_BASE_URL,
+//       CLIENT_URL,
+//     ],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//   })
+// );
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL="http://localhost:5555"
-export const CLIENT_URL="http://localhost:5173"
+export const API_BASE_URL = "https://pen-nest-backend.vercel.app/";
+export const CLIENT_URL = "http://localhost:5173";
