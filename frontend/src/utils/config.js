e0fe6781dd9843e63629fb57f9a6987a948f54b7
Rahul Batra <97456632+BtraRahul@users.noreply.github.com>
2024-06-29 12:58:14 +0530

    Update config.js

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,2 +1,3 @@
-export const API_BASE_URL="https://pen-nest-backend.vercel.app/"
+export const API_BASE_URL="https://pen-nest-backend.vercel.app"
+// export const API_BASE_URL="http://localhost:5555"
 export const CLIENT_URL="http://localhost:5173"
